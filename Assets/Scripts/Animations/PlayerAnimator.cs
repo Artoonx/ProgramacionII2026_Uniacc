7112eef83d427622602ae3c87d4456622c64bb09
WaruGameDev <60048774+WaruGameDev@users.noreply.github.com>
2026-03-18 16:18:24 -0300

    animaciones y mejoras de movimiento

---
+++ Assets/Scripts/Animations/PlayerAnimator.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Animator animator;
+    public Transform visual;
+    public MovementCharacter movementCharacter;
+    void Update()
+    {
+        float horizontal = rb.linearVelocity.x;
+        float vertical = rb.linearVelocity.y;
+        if(horizontal > 0)
+        {
+            visual.localScale = new Vector3(1, 1, 1);
+        }
+        else if(horizontal < 0)
+        {
+            visual.localScale = new Vector3(-1, 1, 1);
+        }
+
+        animator.SetFloat("Horizontal", Mathf.Abs(horizontal));
+        animator.SetFloat("Vertical", vertical);      
+        animator.SetBool("Grounded", movementCharacter.IsGrounded());
+    }
+}
--- Assets/Scripts/Core/MovementCharacter.cs
+++ Assets/Scripts/Core/MovementCharacter.cs
@@ -7,21 +7,51 @@ public class MovementCharacter : MonoBehaviour
     public float jumpForce = 5f;
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
+    public float lowMultiplier = 0.5f;
+    public float fallMultiplier = 2.5f;
+    public float coyoteTime = 0.2f;
+    private float coyoteTimeCounter;
+    public float prebuffTime = 0.2f;
+    private float prebuffTimeCounter;
+    public bool jumping;
+
 
     // Update is called once per frame
     void Update()
     {
         float xInput = Input.GetAxis("Horizontal");
-
         rb.linearVelocity = new Vector2(xInput * speed, rb.linearVelocity.y);
 
-        if(Input.GetButtonDown("Jump") && IsGrounded())
+        // Prebuffer: registra intención de salto
+        if (Input.GetButtonDown("Jump"))
+            prebuffTimeCounter = prebuffTime;
+        else
+            prebuffTimeCounter -= Time.deltaTime;
+
+        // Coyote time: guarda tiempo desde que dejó el suelo
+        if (IsGrounded())
         {
+            coyoteTimeCounter = coyoteTime; // ✅ Se llena cuando está en suelo
+            jumping = false;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime; // ✅ Se vacía al estar en el aire
+        }
+
+        // Salto: prebuffer + coyote time combinados
+        if (prebuffTimeCounter > 0 && coyoteTimeCounter > 0 && !jumping)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0); // ✅ Resetea velocidad vertical
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            jumping = true;
+            coyoteTimeCounter = 0;  // ✅ Evita doble salto
+            prebuffTimeCounter = 0;
         }
     }
-    bool IsGrounded()
+    public bool IsGrounded()
     {
+        
         return Physics2D.OverlapCircle(groundCheck.position, 
         groundCheckRadius, LayerMask.GetMask("Ground"));
     }
